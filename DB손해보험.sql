faa9bb0ae03523abea22ffd21b32b246717584a1
김승환 <rlatmdghks6287@naver.com>
2024-08-14 17:16:55 +0900

    no message

---
+++ DB손해보험.sql
@@ -0,0 +1,24 @@
+CREATE TABLE tbl_todo (
+   tno      number,
+   title    varchar2(100) not null,
+   duedate    date not null,
+   finished char(1)   
+);
+
+select * from tbl_todo;
+
+create sequence seq_todo;
+
+insert into tbl_todo (tno, title, duedate, finished)
+    values (seq_todo.nextval, 'test3', '2024-08-14', 'N');
+insert into tbl_todo (tno, title, duedate, finished)
+    values (seq_todo.nextval, 'test4', '2024-08-15', 'Y');
+
+drop table tbl_todo;
+drop sequence seq_todo;
+
+commit;
+
+select * from tbl_todo
+where tno = 2;
+
